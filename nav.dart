a29e24409e63b0c2207c28f538554f7b8b2d2c48
Melias96 <77294683+Melias96@users.noreply.github.com>
2021-11-19 11:51:59 -0500

    Add files via upload

---
+++ nav.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+import 'package:finalapp/home_screen.dart';
+
+class Nav extends StatefulWidget {
+  @override
+  _NavState createState() => _NavState();
+}
+
+class _NavState extends State<Nav> {
+  int _selectedIndex = 0;
+  List<Widget> _widgetOptions = <Widget>[
+    Home(),
+    Text('Messgaes Screen'),
+    Text('Search Screen'),
+  ];
+
+  void _onItemTap(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Final Project'),
+      ),
+      body: Center(
+        child: _widgetOptions.elementAt(_selectedIndex),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.home,
+            ),
+            title: Text(
+              'Forum',
+            ),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.message,
+            ),
+            title: Text(
+              'Chat',
+            ),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.search
+            ),
+            title: Text(
+              'Search',
+            ),
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        onTap: _onItemTap,
+        selectedFontSize: 13.0,
+        unselectedFontSize: 13.0,
+      ),
+    );
+  }
+}
